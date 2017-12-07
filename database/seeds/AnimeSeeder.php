70acadfba59415c2446c268f07ad4af764744f7e
Luigui Mendoza <ludk17@gmail.com>
2017-12-07 06:43:43 -0500

    fix

--- database/seeds/AnimeSeeder.php
+++ database/seeds/AnimeSeeder.php
@@ -18,61 +18,61 @@ class AnimeSeeder extends Seeder
 
         Anime::create([
             'nombre' => 'Bleach',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/bleach.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Code Geass',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/code_geass.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Death Note',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/death_note.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Dragon Ball Z',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/dragon_ball_z.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Fairy Tail',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/fairy_tail.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Naruto Shippuden',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/naruto_shippuden.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'One Piece',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/one_piece.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Shingeki no kyojin',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/shingeki_no_kyojin.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Super Campeones',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/super_campeones.jpg')
         ]);
 
         Anime::create([
             'nombre' => 'Zenki',
-            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'descripcion' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
             'imagen' => url('img/zenki.jpg')
         ]);
     }
